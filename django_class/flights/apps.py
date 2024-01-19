a51418c4d9294933748eb17ffe2629c3b17bf19f
Ana Clara <ana05clara24@gmail.com>
2024-01-19 11:48:57 -0300

    Reinitializing the application

--- django_class/flights/apps.py
+++ django_class/flights/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TasksListConfig(AppConfig):
+class FlightsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tasks_list'
+    name = 'flights'
