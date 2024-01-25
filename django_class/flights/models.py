63619562ddf9e6627714972dbfb55b318f503aec
Ana Clara <ana05clara24@gmail.com>
2024-01-25 18:45:34 -0300

    Adding a model and associating it with the django admin and web page

--- django_class/flights/models.py
+++ django_class/flights/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
-class Flight(models.Model):
-    origin = models.CharField(max_length = 65)
+class Airport(models.Model):
+    city = models.CharField(max_length = 30)
 
-    destination = models.CharField(max_length = 65)
+    code = models.CharField(max_length = 3)
+
+    def __str__(self):
+        return f"{self.city}({self.code})"
+    
+
+class Flight(models.Model):
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name= "departures")
+
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name= "arrivals")
 
     duration = models.IntegerField()
     
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination} duration: {self.duration}"
+    
